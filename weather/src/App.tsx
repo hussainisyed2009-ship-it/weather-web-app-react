82d0d014861b23bad9d9a19acf17bd09afff8f89
hussainisyed2009-ship-it <hussainisyed2009@gmail.com>
2026-01-15 22:04:45 -0500

    .

--- weather/src/App.tsx
+++ weather/src/App.tsx
@@ -1,10 +1,20 @@
+import { useQuery } from "@tanstack/react-query"
+import { getWeather } from "./api"
 
 
 function App() {
 
+  const {data} = useQuery({
+    queryKey: ['weather'],
+    queryFn: () => getWeather({lat: 10, lon:25})
+  })
+
   return (
-    <></>
+    <>{JSON.stringify(data)}</>
   )
 }
 
 export default App
+
+// styling - tailwind
+// data fetching - Tanstack Query (React Query)
